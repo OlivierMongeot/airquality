2daa7b2f36a01fefad78a885b5a574537aea9e32
OlivierMongeot <olivier.mongeot@gmail.com>
2021-07-08 16:38:46 +0100

    disable error reporting

--- core/class/airquality.class.php
+++ core/class/airquality.class.php
@@ -1,8 +1,8 @@
 <?php
 // Setup Error : only dev 
-error_reporting(E_ALL);
-ini_set('ignore_repeated_errors', TRUE);
-ini_set('display_errors', TRUE);
+// error_reporting(E_ALL);
+// ini_set('ignore_repeated_errors', TRUE);
+// ini_set('display_errors', TRUE);
 
 /* This file is part of Jeedom.
  *
